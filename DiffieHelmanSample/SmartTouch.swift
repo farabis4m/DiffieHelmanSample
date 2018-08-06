9b3eceb3701d3ff311281dbb8d9a28047c9237f0
Sanu <sanu@farabi.ae>
2018-08-06 15:48:12 +0400

    Adds pods

---
+++ DiffieHelmanSample/SmartTouch.swift
@@ -0,0 +1,25 @@
+//
+//  SmartTouch.swift
+//  DiffieHelmanSample
+//
+//  Created by Sanu Sathyaseelan on 8/6/18.
+//  Copyright © 2018 Farabi. All rights reserved.
+//
+
+
+import ObjectMapper
+
+class Object: Mappable {
+    
+    required init?(map: Map) {}
+
+    init() {}
+
+    func mapping(map: Map) {}
+}
+
+
+class SmartTouch: Object {
+    
+    
+}
---
+++ Podfile
@@ -0,0 +1,13 @@
+platform :ios, '9.0'
+
+use_frameworks!
+
+def pods
+    pod 'KSAUtils', git: 'http://10.10.10.84:8111/scm/git/KSAUtils', branch: 'master-touchId'
+    pod 'FTLocalizationManager', :git => 'https://github.com/farabis4m/FTLocalizationManager.git'
+    pod 'ObjectMapper', '~> 3.1'
+end
+
+target 'DiffieHelmanSample' do
+ pods
+end
